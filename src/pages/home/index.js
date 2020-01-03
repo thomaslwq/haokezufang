689a433cd2a68d21c73ef1ee77b65b5d16996439
thomas.lin <thomaslwq@gmail.com>
2020-01-03 15:05:25 +0800

    init router info

---
+++ src/pages/home/index.js
@@ -0,0 +1,11 @@
+import React, { Component,Fragment } from 'react'
+
+export default class Index extends Component {
+    render() {
+        return (
+            <Fragment>
+                home
+            </Fragment>
+        )
+    }
+}
