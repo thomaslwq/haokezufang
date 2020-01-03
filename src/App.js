c026c4ee1ba72b73da9305acbda45282a5b1cd95
thomas.lin <thomaslwq@gmail.com>
2020-01-03 17:29:59 +0800

    finish tabbar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from "./pages/home"
 import Info from "./pages/info"
 import List from "./pages/list"
 import Profile from "./pages/profile"
+//5. 引入 一個HKLayout 作我們的tabbar
+import HKLayout from "./components/HKLaout"
 
 //1. rcc 新建一個 App 組件
 export default class App extends Component {
@@ -16,10 +18,10 @@ export default class App extends Component {
           4. 配置路由信息
         */}
         <Router>
-          <Route path="/" exact render={(props) => <Home {...props}></Home>}></Route>
-          <Route path="/info" exact render={(props) => <Info {...props}></Info>}></Route>
-          <Route path="/list" exact render={(props) => <List {...props}></List>}></Route>
-          <Route path="/profile" exact render={(props) => <Profile {...props}></Profile>}></Route>
+          <Route path="/" exact render={() => <HKLayout><Home /></HKLayout>} />
+          <Route path="/list" exact render={() => <HKLayout><List /></HKLayout>} />
+          <Route path="/info" exact render={() => <HKLayout><Info /></HKLayout>} />
+          <Route path="/profile" exact render={() => <HKLayout><Profile /></HKLayout>} />
         </Router>
       </Fragment>
     )
---
+++ src/components/HKLaout/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import { TabBar } from 'antd-mobile';
+//1. 引入一個withRouter 組件
+import { withRouter } from "react-router-dom"
+
+
+/* 
+    用來顯示底部的tabbar
+*/
+ class HKLayout extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div style={{ position: 'fixed', height: '100%', width: '100%', top: 0 }}>
+                <TabBar
+                    unselectedTintColor="#949494"
+                    tintColor="#21B97A"
+                    barTintColor="white"
+                >
+                    <TabBar.Item
+                        title="首頁"
+                        key="Life"
+                        icon={<i className="iconfont icon-ind"></i>
+                        }
+                        selectedIcon={<i className="iconfont icon-ind"></i>
+                        }
+                        selected={this.props.match.path === '/'}
+                        onPress={() => {
+                            this.props.history.push("/");
+                        }}
+                    >
+                        {this.props.children}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={
+                            <i className="iconfont icon-findHouse"></i>
+                        }
+                        selectedIcon={<i className="iconfont icon-findHouse"></i>
+                        }
+                        title="找房"
+                        key="findHouse"
+                        selected={this.props.match.path === '/list'}
+                        onPress={() => {
+                            this.props.history.push("/list");
+                        }}
+                    >
+                        {this.props.children}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={
+                            <i className="iconfont icon-infom"></i>
+                        }
+                        selectedIcon={
+                            <i className="iconfont icon-infom"></i>
+                        }
+                        title="咨詢"
+                        key="info"
+                        selected={this.props.match.path=== '/info'}
+                        onPress={() => {
+                            this.props.history.push("/info");
+                        }}
+                    >
+                        {this.props.children}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={<i className="iconfont icon-myinfo"></i>}
+                        selectedIcon={<i className="iconfont icon-myinfo"></i>}
+                        title="我的"
+                        key="my"
+                        selected={this.props.match.path === '/profile'}
+                        onPress={() => {
+                            this.props.history.push("/profile");
+                        }}
+                    >
+                        {this.props.children}
+                    </TabBar.Item>
+                </TabBar>
+            </div>
+        );
+    }
+}
+//2. 將HKLayout 進行包裝
+export default  withRouter(HKLayout)
